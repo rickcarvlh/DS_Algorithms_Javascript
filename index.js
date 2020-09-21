9fc8687ee60596d234723cec015870b0de53d917
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-09-21 16:17:30 +0100

    Basics of stacks implemented

--- index.js
+++ index.js
@@ -1,7 +1,69 @@
-/*Stacks */
+/* Stacks */
 
 // functions: push pop peek length
 
 let letters = [] // this is our stack
 
+let word = "racecar" // -> palindrome
+
+let rword = "";
+
+// put letters of word into stack
+for (let i = 0; i < word.length; i++) {
+    letters.push(word[i])
+}
+
+// pop off the stack in reverse  order
+for (let i = 0; i < word.length; i++) {
+    rword += letters.push()
+}
+
+if (rword === word) {
+    console.log(word + " is a palindrome");
+} else {
+    console.log(word + " is not a palindrome");
+}
+
+// Creates a Stack
+let Stack = function () {
+    this.count = 0;
+    this.storage = {};
+
+    // Adds a value onto the end of the stack
+    this.push = function (value) {
+        this.storage[this.count] = value;
+        this.count++;
+        console.log("this count ++ " + this.count);
+    }
+
+    // Removes and returns the value at the end of the stack
+    this.pop = function () {
+        if (this.count === 0) {
+            return undefined
+        }
+
+        this.count--;
+        let result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result
+    }
+
+    this.size = function () {
+        return this.count;
+    }
+
+    // return the value at the end of the stack
+    this.peek = function () {
+        return this.storage[this.count - 1];
+    }
+
+}
+
+let myStack = new Stack()
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
 
